1de6ee309560d8cffc4bdec0f6e67ce7e1d35646
mis267 <deness67@live.fr>
2020-10-12 13:03:06 +0200

    last push

--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -3,8 +3,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#define CONTENT "#include <stdio.h>%1$c#include <sys/types.h>%1$c#include <sys/stat.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c#define CONTENT %2$c%3$s%2$c%1$c#define CODE()({int fd;if ((fd = open(%2$cGrace_kid.c%2$c, O_CREAT | O_RDWR, 0666)) >= 0){dprintf(fd, CONTENT, 10, 34, CONTENT);}else dprintf(2, %2$cGrace: Error: Can not open file Grace_kid.c%%1$c%2$c, 10);})%1$c#define START int main() {CODE();}%1$c%1$c/*%1$c    This is a comment%1$c*/%1$cSTART%1$c"
-#define CODE()({int fd;if ((fd = open("Grace_kid.c", O_CREAT | O_RDWR, 0666)) >= 0){dprintf(fd, CONTENT, 10, 34, CONTENT);}else dprintf(2, "Grace: Error: Can not open file Grace_kid.c%1$c", 10);})
+#define CONTENT "#include <stdio.h>%1$c#include <sys/types.h>%1$c#include <sys/stat.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c#define CONTENT %2$c%3$s%2$c%1$c#define CODE()({int fd;if ((fd = open(%2$cGrace_kid.c%2$c, O_CREAT | O_RDWR | O_TRUNC, 0666)) >= 0){dprintf(fd, CONTENT, 10, 34, CONTENT);}else dprintf(2, %2$cGrace: Error: Can not open file Grace_kid.c%%1$c%2$c, 10);})%1$c#define START int main() {CODE();}%1$c%1$c/*%1$c    This is a comment%1$c*/%1$cSTART%1$c"
+#define CODE()({int fd;if ((fd = open("Grace_kid.c", O_CREAT | O_RDWR | O_TRUNC, 0666)) >= 0){dprintf(fd, CONTENT, 10, 34, CONTENT);}else dprintf(2, "Grace: Error: Can not open file Grace_kid.c%1$c", 10);})
 #define START int main() {CODE();}
 
 /*
